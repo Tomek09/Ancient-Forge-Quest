4a1b4481c242d5e04f9a2791cb72156f979b15e4
Tomasz Kierpiec <kierpiec.tomek@gmail.com>
2025-03-23 21:14:36 +0100

    Bonuses and BonusItems

---
+++ Assets/Scripts/Managers/BonusesManager.cs
@@ -0,0 +1,39 @@
+﻿using AncientForgeQuest.Inventories;
+using AncientForgeQuest.Models;
+using AncientForgeQuest.Utility;
+
+namespace AncientForgeQuest.Managers
+{
+    public class BonusesManager : Singleton<BonusesManager>
+    {
+        private InventorySlot[] _playerSlots;
+        
+        private void Start()
+        {
+            var inventory = GameInventoryManager.Instance.Inventory;
+            _playerSlots = inventory.Slots;
+        }
+
+        public bool TryGetBonus(BonusType bonusType, out float value)
+        {
+            // TODO: Big no no no no
+            
+            value = -1;
+            foreach (var slot in _playerSlots)
+            {
+                if (slot.IsEmpty())
+                    continue;
+
+                if (slot.Item.CurrentValue is not BonusItem bonusItem)
+                    continue;
+
+                if (bonusItem.BonusType != bonusType)
+                    continue;
+                
+                value = bonusItem.Value;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Models/BonusItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace AncientForgeQuest.Models
+{
+    [CreateAssetMenu(fileName = "New Bonus Item", menuName = "Models/Items/Bonus")]
+    public class BonusItem : ItemModel
+    {
+        [Header("Bonuses")]
+        [SerializeField] private BonusType _bonusType;
+        [SerializeField] private float _value;
+
+        public BonusType BonusType
+        {
+            get => _bonusType;
+        }
+
+        public float Value
+        {
+            get => _value;
+        }
+
+        public override string GetDescription()
+        {
+            return _bonusType switch
+            {
+                BonusType.IncreasesCraftChance => $"Increases success rate by {Mathf.RoundToInt(_value * 100f)} percentage points",
+                BonusType.ReducesCraftTime => $"Reduces crafting time by {Mathf.RoundToInt(_value)} seconds.",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+}
